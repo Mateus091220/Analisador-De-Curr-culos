0042757ca271858e2a0721c0f180e902d5af005d
Mateus091220 <mateus.m.defarias@gmail.com>
2025-04-08 09:54:44 -0300

    corrige binding para 0.0.0.0 e define porta

--- frontend/script.js
+++ frontend/script.js
@@ -7,7 +7,7 @@ function analisar() {
         return;
     }
 
-    fetch("http://127.0.0.1:5000/analisar", {
+    fetch("https://analisador-curriculo.onrender.com/analisar", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ vaga, curriculo })
